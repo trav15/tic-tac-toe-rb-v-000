435cbaefcd516cbf16f2fbd8bd105a4bf2b3e2be
Travis Tokuyama <travistokuyama@gmail.com>
2018-09-10 17:25:28 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -49,7 +49,21 @@ def turn(board)
   else
     turn(board)
   end
-en
+end
+
+def turn_count(board)
+  counter = 0
+  board.each do |current_token|
+    if current_token == "X" || current_token == "O"
+      counter += 1
+    end
+  end
+  counter
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
 
 
 def won?(board)
